7e05b3250c3345b45d6c33f24346cc1553652068
VahagnCh <vahagnchavushyan@gmail.com>
2024-11-25 18:27:21 -0600

    Funders array to identify ammount funded

--- FundMe.sol
+++ FundMe.sol
@@ -12,10 +12,15 @@ contract FundMe {
 
     uint256 public minimumUsd = 5e18;
 
+    address[] public funders;
+    mapping(address funder => uint256 amountFunded) public addressToAmountFunded; 
+
     function fund() public payable {
         // Allow users to send money
         // Have a minimun money sent $5 USD
         require(getConversionRate(msg.value) >= minimumUsd, "Didn't send enough ETH" ); // 1e18 = 1ETH = 1000000000000000000 = 1 * 10 ** 18
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
         //What is a revert?
         // Undo any action that have been done, and send the remaining gas back
     }
